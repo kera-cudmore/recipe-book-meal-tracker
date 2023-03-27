749f8839f1c375ae647cc7c9b28eb9727bd237ca
Kera Cudmore <kera.cudmore@hotmail.com>
2023-03-27 21:11:46 +0100

    add: add recipe & recipes pages

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,18 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Recipe
 from .forms import RecipeForm
+
+
+class Recipes(ListView):
+    """
+    View All Recipes
+    """
+    template_name = 'recipes/recipes.html'
+    model = Recipe
+    context_object_name = 'recipes'
 
 
 class AddRecipe(LoginRequiredMixin, CreateView):
