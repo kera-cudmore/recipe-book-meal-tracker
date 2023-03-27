0055d5eea2cdb244e8d51b9625c4cb4a15baff13
Kera Cudmore <kera.cudmore@hotmail.com>
2023-03-27 21:40:20 +0100

    add: created recipe detail page & styling

--- recipes/forms.py
+++ recipes/forms.py
@@ -7,35 +7,36 @@ class RecipeForm(forms.ModelForm):
     """
     Form to create a recipe
     """
+
     class Meta:
         model = Recipe
         fields = [
-            'title',
-            'description',
-            'ingredients',
-            'instructions',
-            'image',
-            'image_alt',
-            'meal_type',
-            'cuisine_types',
-            'calories'
+            "title",
+            "description",
+            "ingredients",
+            "instructions",
+            "image",
+            "image_alt",
+            "meal_type",
+            "cuisine_types",
+            "calories",
         ]
 
         ingredients = forms.CharField(widget=RichTextWidget())
         instructions = forms.CharField(widget=RichTextWidget())
 
         widget = {
-            'description': forms.Textarea(attrs={'rows': 5}),
+            "description": forms.Textarea(attrs={"rows": 5}),
         }
 
         labels = {
-            'title': 'Recipe Title',
-            'description': 'Description',
-            'ingredients': 'Recipe Ingredients',
-            'instructions': 'Recipe Instructions',
-            'image': 'Recipe Image',
-            'image_alt': 'Describe Image',
-            'meal_type': 'Meal Type',
-            'cuisine_types': 'Cuisine Type',
-            'calories': 'Calories',
+            "title": "Recipe Title",
+            "description": "Description",
+            "ingredients": "Recipe Ingredients",
+            "instructions": "Recipe Instructions",
+            "image": "Recipe Image",
+            "image_alt": "Describe Image",
+            "meal_type": "Meal Type",
+            "cuisine_types": "Cuisine Type",
+            "calories": "Calories",
         }
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -10,19 +10,31 @@ class Recipes(ListView):
     """
     View All Recipes
     """
-    template_name = 'recipes/recipes.html'
+
+    template_name = "recipes/recipes.html"
     model = Recipe
-    context_object_name = 'recipes'
+    context_object_name = "recipes"
+
+
+class RecipeDetail(DetailView):
+    """
+    View a single recipe
+    """
+
+    template_name = "recipes/recipe_detail.html"
+    model = Recipe
+    context_object_name = "recipe"
 
 
 class AddRecipe(LoginRequiredMixin, CreateView):
     """
     Add Recipe View
     """
-    template_name = 'recipes/add_recipe.html'
+
+    template_name = "recipes/add_recipe.html"
     model = Recipe
     form_class = RecipeForm
-    success_url = '/recipes/'
+    success_url = "/recipes/"
 
     def form_valid(self, form):
         form.instance.user = self.request.user
