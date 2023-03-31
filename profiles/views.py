e7102fd55d159b2d0cbf1561426af2cda6fd038a
Kera Cudmore <kera.cudmore@hotmail.com>
2023-03-31 17:49:13 +0100

    add: logic for profiles urls & added link to navbar

--- profiles/views.py
+++ profiles/views.py
@@ -1,1 +1,16 @@
-from django.shortcuts import 
+from django.views.generic import TemplateView
+
+from .models import Profile
+
+
+class Profiles(TemplateView):
+    """User Profile View"""
+    template_name = "profiles/profile.html"
+
+    def get_context_data(self, **kwargs):
+        profile = Profile.objects.get(user=self.kwargs["pk"])
+        context = {
+            'profile': profile
+        }
+
+        return context
